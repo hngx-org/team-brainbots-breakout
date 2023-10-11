8f89915c2db631173577519d757897b83fcf81ff
olasunkanmi beckley <beckleysunkanmi@gmail.com>
2023-10-11 22:01:02 +0100

    made the sprites reusable

--- lib/src/game/managers/level_manager.dart
+++ lib/src/game/managers/level_manager.dart
@@ -1,27 +1,44 @@
 import 'package:brainbots_breakout/src/game/breakout.dart';
 import 'package:flame/components.dart';
 
-class BallSpeed{
-  Vector2 speed;
+class BallSettings{
+  Vector2 velocity;
   Vector2 gravity;
-  BallSpeed({required this.speed, required this.gravity});
+  BallSettings({required this.velocity, required this.gravity});
+}
+
+class PaddleSettings{
+  double speedMultiplier;
+  PaddleSettings({required this.speedMultiplier});
+}
+
+class BrickSettings{
+  int strength;
+  int numBricks;
+  BrickSettings({required this.strength, required this.numBricks});
 }
 
 class LevelManager extends Component with HasGameRef<Breakout>{
   int level;
   LevelManager({this.level = 1});
 
-  final Map<int, BallSpeed> ballConfig =  {
-    1: BallSpeed(speed: Vector2(0, 400), gravity: Vector2(0, 20)),
+  final Map<int, BallSettings> ballConfig =  {
+    1: BallSettings(velocity: Vector2(0, 400), gravity: Vector2(0, 2)),
   };
 
-  final Map<int, int> brickConfig = {
-    1: 2,
+  final Map<int, PaddleSettings> paddleConfig = {
+    1: PaddleSettings(speedMultiplier: 100),
   };
 
-  Vector2 get ballSpeed{
+  final Map<int, BrickSettings> brickConfig = {
+    1: BrickSettings(strength: 1, numBricks: 10),
+  };
+
+  
+
+  Vector2 get initialVelocity{
     if (ballConfig[level] != null){
-      return ballConfig[level]!.speed;
+      return ballConfig[level]!.velocity;
     }
     return Vector2.zero();
   }
@@ -33,10 +50,24 @@ class LevelManager extends Component with HasGameRef<Breakout>{
     return Vector2.zero();
   }
 
+  double get paddleSpeedMultiplier{
+    if (paddleConfig[level] != null){
+      return paddleConfig[level]!.speedMultiplier;
+    }
+    return 0;
+  }
+
   int get brickStrength{
     if (brickConfig[level] != null){
-      return brickConfig[level]!;
+      return brickConfig[level]!.strength;
     }
-    return 1;
+    return 0;
+  }
+
+  int get numBricks{
+    if (brickConfig[level] != null){
+      return brickConfig[level]!.numBricks;
+    }
+    return 0;
   }
 }
--- lib/src/game/sprites/paddle.dart
+++ lib/src/game/sprites/paddle.dart
@@ -1,35 +1,34 @@
-import 'package:brainbots_breakout/src/game/managers/managers.dart';
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
+import 'package:flame/extensions.dart';
 
 enum PaddleDirection {left, center, right}
 class Paddle extends SpriteComponent with HasGameRef, CollisionCallbacks, DragCallbacks{
-  GameManager gameManager;
-  LevelManager levelManager;
+  Vector2 paddleSize;
+  Vector2 paddlePosition;
+  double speedMultiplier;
 
   Paddle({
-    required this.gameManager,
-    required this.levelManager,
+    required this.paddleSize,
+    required this.paddlePosition,
+    required this.speedMultiplier,
   });
   
   late ShapeHitbox hitbox;
   late PaddleDirection direction;
-  // late double paddleBoost;
+  late double paddleBoost;
+  bool canMove = false;
 
   @override
   Future<void> onLoad() async {
     await super.onLoad();
+    sprite = await gameRef.loadSprite('paddle.png');
+    size = paddleSize;
+    position = paddlePosition;
+
     direction = PaddleDirection.center;
-    sprite = await gameRef.loadSprite(
-      'paddle.png',
-    );
-    width *= 0.2;
-    height *= 0.2;
-    position = Vector2(
-      game.size.x * 0.5 - width/2,
-      game.size.y * 0.9
-    );
+
     hitbox = RectangleHitbox();
     add(hitbox);
   }
@@ -37,28 +36,27 @@ class Paddle extends SpriteComponent with HasGameRef, CollisionCallbacks, DragCa
   @override
   void onDragUpdate(DragUpdateEvent event){
     super.onDragUpdate(event);
-    if(!(position.x + event.delta.x < 0) && !(position.x + width + event.delta.x > game.size.x)){
-      position.x += event.delta.x;
-    }
-    // paddleBoost = 2 * (event.devicePosition.x / game.size.x) - 1;
-    // print(paddleBoost);
-    if(event.delta.x < 0){
-      direction = PaddleDirection.left;
-    }
-    else if(event.delta.x > 0){
-      direction = PaddleDirection.right;
-    }
-    else{
-      direction = PaddleDirection.center;
+    if(canMove){
+      if(!(position.x + event.delta.x < 0) && !(position.x + width + event.delta.x > game.size.x)){
+          position.x += event.delta.x;
+        }
+        paddleBoost = 2 * (event.devicePosition.x / game.size.x) - 1;
+        // print(paddleBoost);
+        if(event.delta.x < 0){
+          direction = PaddleDirection.left;
+        }
+        else if(event.delta.x > 0){
+          direction = PaddleDirection.right;
+        }
+        else{
+          direction = PaddleDirection.center;
+        }
     }
   }
 
   @override
   void onDragStart(DragStartEvent event){
     super.onDragStart(event);
-    if(!gameManager.isPlaying){
-      gameManager.state = GameState.intro;
-    }
   }
 
   @override
