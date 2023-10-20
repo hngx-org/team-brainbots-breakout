9625d9e7549fd43b806b7dde17a74222252f0234
Olasunkanmi Beckley <48617652+o-beckley@users.noreply.github.com>
2023-10-20 12:19:47 +0100

    Revert "Revert "added slow ball fast ball power ups. set collision activities for laser with bricks, created power up flash message anytime user gets a power up, created brick patterns for that are toggle randomly for every level. added timer to keep track of game length, adjusted splash screen UI. created pattern for how many types of power ups gets spwaned per level""

--- lib/src/config/user_config.dart
+++ lib/src/config/user_config.dart
@@ -3,6 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class UserConfig extends ChangeNotifier{
   late ValueNotifier<int> highScore;
+  late ValueNotifier<int> totalGameTime;
   late ValueNotifier<int> levelsUnlocked;
   late ValueNotifier<bool> musicOn;
   late ValueNotifier<bool> sfxOn;
@@ -12,6 +13,7 @@ class UserConfig extends ChangeNotifier{
     prefs = await SharedPreferences.getInstance();
 
     highScore = ValueNotifier(prefs.getInt('highScore') ?? 0);
+    totalGameTime = ValueNotifier(prefs.getInt('totalGameTime') ?? 0);
     levelsUnlocked = ValueNotifier(prefs.getInt('levelsUnlocked') ?? 1);
 
     musicOn = ValueNotifier(prefs.getBool('musicOn') ?? true);
@@ -20,6 +22,10 @@ class UserConfig extends ChangeNotifier{
 
     highScore.addListener(() {
       prefs.setInt('highScore', highScore.value);
+    });
+
+    totalGameTime.addListener(() {
+      prefs.setInt('totalGameTime', totalGameTime.value);
     });
     levelsUnlocked.addListener(() {
       prefs.setInt('levelsUnlocked', levelsUnlocked.value);
